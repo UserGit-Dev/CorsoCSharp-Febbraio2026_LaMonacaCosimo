ad2878e7aec1d1be2976c01d79bb618bbe6d0300
UserGit-Dev <welcometorapture@hotmail.it>
2026-02-17 17:54:05 +0100

    2h

---
+++ 2026-02-17/prova1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Inserisci il primo numero: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Inserisci il secondo numero: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b){
+    Console.WriteLine($"{a} è più grande di {b}");
+} else {
+    Console.WriteLine($"{b} è più grande di {a}");
+}
+
+Console.WriteLine();
+
+// Ciclo While
+bool continua = true;
+while (continua) //finché "continua" rimane true il codice viene ripetuto
+{
+    Console.WriteLine("Ciclo in esecuzione");
+    // continua = bool.Parse(Console.ReadLine()!);
+    continua = bool.Parse(Console.ReadLine()!);
+}
+
+int continua2 = 0;
+while (continua2 < 10) //finché "continua" rimane true il codice viene ripetuto
+{
+    Console.WriteLine("Ciclo in esecuzione");
+    continua2 += 1;
+    Console.WriteLine(continua2);
+}
