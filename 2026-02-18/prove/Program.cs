71623a9ad5cfd89a67a1655fcc0fb5c48884c333
UserGit-Dev <welcometorapture@hotmail.it>
2026-02-18 12:58:34 +0100

    3c

---
+++ 2026-02-18/prove/Program.cs
@@ -0,0 +1,17 @@
+﻿bool continua = true;
+do //prima esegue il codice poi controlla se la condizione è vera
+{
+    Console.WriteLine("Quanti anni hai: ");
+    int anni = int.Parse(Console.ReadLine()!);
+    if (anni >= 18) {
+        continua = false;
+        Console.Clear();
+        Console.WriteLine("Sei maggiorenne, arrivederci!");
+        Console.WriteLine();
+    } else {
+        Console.Clear();
+        Console.WriteLine("Non sei maggiorenne, ritenta!");
+        Console.WriteLine();
+    }
+}
+while (continua);
