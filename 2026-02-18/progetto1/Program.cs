3677c0b49077e7c9b61e352208fde7bf4fa18427
UserGit-Dev <welcometorapture@hotmail.it>
2026-02-18 12:56:47 +0100

    3

---
+++ 2026-02-18/progetto1/Program.cs
@@ -0,0 +1,61 @@
+﻿// 1.
+bool flag = true;
+double saldo = 0;
+
+while (flag)
+{
+    Console.WriteLine("Benvenuto!");
+    Console.WriteLine("Effettua una delle seguenti operazioni: ");
+    Console.WriteLine("1. Visualizza saldo");
+    Console.WriteLine("2. Deposita denaro");
+    Console.WriteLine("3. Preleva denaro");
+    Console.WriteLine("4. Esci");
+
+    int opzione = int.Parse(Console.ReadLine()!);
+
+    if (opzione != 4)
+    {
+        switch (opzione)
+        {
+            case 1:
+                Console.Clear();
+                Console.WriteLine($"Saldo attaule: {saldo}€");
+                Console.WriteLine();
+                break;
+            case 2:
+                Console.Clear();
+                Console.WriteLine("Inserire quantitativo da depositare: ");
+                saldo += double.Parse(Console.ReadLine()!);
+                Console.Clear();
+                Console.WriteLine($"Operazione completata, saldo attaule: {saldo}€");
+                Console.WriteLine();
+                break;
+            case 3:
+                Console.Clear();
+                Console.WriteLine("Inserire il quantitativo da prelevare: ");
+                double qntPrelievo = double.Parse(Console.ReadLine()!);
+                if (qntPrelievo < saldo) {
+                    saldo -= qntPrelievo;
+                    Console.Clear();
+                    Console.WriteLine($"Prelievo effettuato con successo, saldo attaule: {saldo}€");
+                    Console.WriteLine();
+                    break;
+                } else {    
+                    Console.Clear();
+                    Console.WriteLine("Errore, somma da prelevare troppo elevata.");
+                    Console.WriteLine();
+                    break;
+                }
+            default:
+                Console.Clear();
+                Console.WriteLine("Errore, opzione non disponibile!");
+                Console.WriteLine();
+                break;
+        }
+    }
+    else { 
+        flag = false;
+        Console.Clear();
+        Console.WriteLine("Operazione conclusa. Arrivederci!"); 
+    }
+}
---
+++ 2026-02-18/progetto2/Program.cs
@@ -0,0 +1,55 @@
+﻿// 1.
+int tentativi = 3;
+string password = "1234";
+
+while (tentativi > 0)
+{
+    Console.WriteLine("Inserisci la password corretta: ");
+    Console.WriteLine($"Tentivi -> {tentativi}");
+    Console.Write("Password: ");
+    string inPassword = Console.ReadLine()!;
+
+    if (inPassword == password) {
+        Console.Clear();
+        Console.WriteLine("Password indovinata!");
+        Console.WriteLine();
+    } else if (inPassword != password && tentativi == 1)
+    {
+        tentativi--;
+        Console.Clear();
+        Console.WriteLine("Password inserita non corretta. Sessione conclusa.");
+        Console.WriteLine();
+    } 
+    else {
+        tentativi--;
+        Console.Clear();
+        Console.WriteLine("Password inserita non corretta, riprova.");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine();
+
+// 2. 
+bool flag1 = true;
+int totale = 0;
+
+while (flag1)
+{
+    Console.Write("Inserisci numero da aggiungere (int): ");
+    int numAggiungere = int.Parse(Console.ReadLine()!);
+
+    if (numAggiungere == 0)
+    {
+        flag1 = false;
+        Console.Clear();
+        Console.WriteLine($"Operazione conclusa, totale: {totale}");
+        Console.WriteLine();
+    } else
+    {
+        totale += numAggiungere;
+        Console.Clear();
+        Console.WriteLine($"Somma aggiunta, totale: {totale}");
+        Console.WriteLine();
+    }
+}
